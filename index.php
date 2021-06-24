d6df989a2df395b593d28bd9934d01470638e003
JuniorJuniorProgramer <86437149+JuniorJuniorProgramer@users.noreply.github.com>
2021-06-25 00:14:27 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,15 @@
 <!DOCTYPE html>
-<html lang="EN">
-<body>
-<?php
-echo "<h1>Welcome!</h1>";
-?>
-</body>
+<html>
+  <head>
+    <title>PHP Store</title>
+  </head>
+  <body>
+    <?php
+      $name = "PHP Store";
+      $credit = 1000; 
+
+      echo "<h1>Welcome to ".$name."!</h1>";
+      echo "<h2>You have $".$credit." in your wallet.</h2>";
+    ?>
+  </body>
 </html>
