b6694f764118c82ebebab46e18db9c25408e926d
JuniorJuniorProgramer <86437149+JuniorJuniorProgramer@users.noreply.github.com>
2021-06-25 00:20:03 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,25 +1,32 @@
 <!DOCTYPE html>
 <html>
-  <head>
+<head>
     <title>PHP Store</title>
-  </head>
-  <body>
-    <?php
-	    $name = "PHP Store";
-			$credit = 1000; 
+</head>
+<body>
+<?php
+$name = "PHP Store";
+$credit = 1000;
 
-	    echo "<h1>Welcome to ".$name."!</h1>";
-	    echo "<h2>You have $".$credit." in your wallet.</h2>";
+echo "<h1>Welcome to ".$name."!</h1>";
+echo "<h2>You have $".$credit." in your wallet.</h2>";
 
-		  $products['Computer']=750;
-	    $products['Car']=15000;
-	    $products['iPhone']=1000;
-	    $products['Toaster']=75;
+$products['Computer']=750;
+$products['Car']=15000;
+$products['iPhone']=1000;
+$products['Toaster']=75;
 
-	    foreach($products as $key => $value){
-		    echo "<p>The ".$key." costs ".$value."</p>";
-	    }
+foreach($products as $key => $value){
+    echo "<p>The ".$key." costs ".$value."</p>";
+}
 
-    ?>
-  </body>
+echo "<h2>Items you can afford</h2>";
+
+foreach($products as $key => $value){
+    if($value <= $credit ){
+        echo "<p>".$key."</p>";
+    }
+}
+?>
+</body>
 </html>
